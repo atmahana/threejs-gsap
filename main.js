390efe7923d17c4993f419cf7040497252ace5fb
Adham <zubairadham.ar@gmail.com>
2023-05-13 22:32:43 +0800

    fix responsive font size

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function init() {
 
   const scene = new THREE.Scene();
   scene.background = new THREE.Color(COLORS.light);
-  scene.fog = new THREE.Fog(COLORS.light, 10, 15);
+  scene.fog = new THREE.Fog(COLORS.light, 1, 17);
 
   const size = {
     width: 0,
@@ -39,14 +39,16 @@ function init() {
 
   const directionalLight = new THREE.DirectionalLight(COLORS.light, 2);
   directionalLight.castShadow = true;
-  directionalLight.shadow.camera.far = 30;
+  directionalLight.shadow.camera.far = 50;
   directionalLight.shadow.mapSize.set(1024, 1024);
   directionalLight.position.set(3, 10, 3);
-    directionalLight.target.updateMatrixWorld();
-//   directionalLight.target.position.set();
-  //   const dlHelper = new THREE.DirectionalLightHelper(directionalLight, 2, "black");
-  //   dlHelper.update();
-  //   scene.add(dlHelper);
+//   const dlHelper = new THREE.DirectionalLightHelper(
+//     directionalLight,
+//     2,
+//     "black"
+//   );
+//   dlHelper.update();
+//   scene.add(dlHelper);
 
   const hemiLight = new THREE.HemisphereLight(COLORS.sky, COLORS.ground, 0.5);
 
@@ -64,38 +66,16 @@ function init() {
 
   scene.add(floor);
 
-  const onResize = () => {
-    size.width = container.clientWidth;
-    size.height = container.clientHeight;
-
-    camera.aspect = size.width / size.height;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(size.width, size.height);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-  };
-
-  window.addEventListener("resize", onResize);
-  onResize();
-
-  // function for re-rendering/animating the scene
-  function tick() {
-    camera.lookAt(cameraTarget);
-    renderer.render(scene, camera);
-    window.requestAnimationFrame(() => tick());
-  }
-  tick();
-
-  const models = {};
+  let models = {};
 
   const toLoad = [
-    { name: "eva01", group: new THREE.Group(), file: "/eva-01.glb" },
-    { name: "eva02", group: new THREE.Group(), file: "/eva-02.glb" },
+    { name: "eva01", group: new THREE.Group(), file: "/models/eva-01.glb" },
+    { name: "eva02", group: new THREE.Group(), file: "/models/eva-02.glb" },
   ];
 
   function setupAnimation() {
     models.eva02.position.x = 100;
-    models.eva01.position.z = -3;
+    models.eva01.position.set(2, 0, -1);
     ScrollTrigger.matchMedia({
       "(prefers-reduced-motion: no-preference)": desktopAnimation,
     });
@@ -117,24 +97,14 @@ function init() {
     });
 
     // Section 2
-    tl.to(camera.position, { x: 2, y: 5, z: 2 }, ".second");
+    tl.to(camera.position, { x: 3.5, y: 6, z: 2 }, ".second");
+    tl.to(models.eva01.position, { x: 2, z: -2 }, ".second");
+    tl.to(models.eva01.rotation, { y: -2 }, ".second");
 
     // Section 3
-
+    tl.to(models.eva01.rotation, { y: 0 }, ".third");
     tl.to(camera.position, { x: -5, y: 5, z: 5 }, ".third");
     tl.to(models.eva01.position, { x: -5.7, z: 4.5 }, ".third");
-
-    // // Section 4
-    // section += 1;
-    // tl.to(camera.position, { x: 5, y: 5, z: 0 }, section);
-
-    // // Section 5
-    // section += 1;
-    // tl.to(camera.position, { x: 0, y: 5, z: 55}, section);
-
-    // // Section 6
-    // section += 1;
-    // tl.to(camera.position, { x: 1, y: 5, z: 0 }, section);
   }
 
   const LoadingManager = new THREE.LoadingManager(() => {
@@ -163,5 +133,29 @@ function init() {
       (err) => console.log(err)
     );
   });
+
+  const onResize = () => {
+    size.width = container.clientWidth;
+    size.height = container.clientHeight;
+
+    camera.aspect = size.width / size.height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(size.width, size.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  };
+
+  window.addEventListener("resize", onResize);
+  onResize();
+
+  const target = new THREE.Object3D();
+  scene.add(target);
+  // function for re-rendering/animating the scene
+  function tick() {
+    camera.lookAt(cameraTarget);
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(() => tick());
+  }
+  tick();
 }
 init();
